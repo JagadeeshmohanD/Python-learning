22deb6b424c32094666cae6f71aa5276f4c2bd85
jagadeeshmohand <jagadeeshdm87@gmail.com>
2020-07-21 17:02:16 +0530

    python

---
+++ LearnPython/Variables.py
@@ -0,0 +1,24 @@
+# from types import DictionaryType
+# from Tools.webchecker.tktools import boolean
+# number = 500.0;
+# number1 = 50;
+# b = "Hello";
+# a = 'This is a string';
+# print(type(number),number);
+# print(type(a),a);
+# print(type(b),b);
+# x=[1,2,3,4,5]
+# x.append(6);
+# print(type(x),x[2:5]);
+# y={'id':1,'name':'alex'};
+# print(type(y),y);
+# Dictionary = dict(
+#     one =1,two='two');
+# print(type(Dictionary),Dictionary);
+boolean = True;
+a1,b1,c1=0,1,2
+if a1== b1:
+    print(True)
+else:
+    print(False)
+
